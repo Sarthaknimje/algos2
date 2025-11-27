467f06ed0dbbdf663440f97bfc879d7f80156616
Sarthak Nimje <sarthaknimje@gmail.com>
2025-11-27 19:33:17 +0530

    fix: Add missing Link import in CreatorProfile.tsx

--- contentvault/src/pages/CreatorProfile.tsx
+++ contentvault/src/pages/CreatorProfile.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { 
   FileText, 
   Download, 
